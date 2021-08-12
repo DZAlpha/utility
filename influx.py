d93a851a57bb1d18a82253117c1820cb94b0245c
SWi98 <45608842+SWi98@users.noreply.github.com>
2021-08-12 11:33:57 +0200

    print on save

--- influx.py
+++ influx.py
@@ -50,6 +50,7 @@ class InfluxClient:
         self.query_api = self.client.query_api()
         
     def save(self, currency, metric, time, **kwargs):
+        print("Save called")
         p = influxdb_client.Point(currency).tag("metric", metric).time(time) #time must be pd.to_datetime(xxx, unit='ms')
         for field in kwargs:
             p = p.field(field, kwargs[field])
